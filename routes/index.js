02098143c79e7d8050a91814cd539dc939c27b92
Michael Bernstein <msb@cs.stanford.edu>
2014-02-19 21:50:45 -0500

    initial setup for index.js

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-	projects['grid'] = true;
+	var grid = true; // flip between true and false to test grid layout
+
+	// your code goes here
+
   	res.render('index', projects);
 };
